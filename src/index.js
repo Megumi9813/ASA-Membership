ce1e492379816da78285062ae459c3ceec3a4c4d
MegumiIgarashi <i.imeguumi@gmail.com>
2022-08-19 17:26:46 -0700

    back

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserContectProvider>
-      <Router basename='/asa-membership/'>
+      <Router>
         <App />
       </Router>
     </UserContectProvider>
